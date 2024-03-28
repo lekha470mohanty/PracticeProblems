7a368819b025d82857281f7c9bba59e96ec91094
lekha470mohanty <lekha470mohanty@gmail.com>
2024-03-28 12:23:35 +0530

    Second Commit

--- src/collectionPractice/HashMapPractice.java
+++ src/collectionPractice/HashMapPractice.java
@@ -7,8 +7,8 @@ public class HashMapPractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HashMap<String,String> hm=new HashMap<String, String>();
-		hm.put("Name", "Lekha");
-		hm.put("Name1", "Lekhgga");
+		hm.put("Name", "Lekhdxcfvgbha");
+		hm.put("Name1", "Lekhhgfdfghbnbvbgga");
 		hm.put(null, null);
 		System.out.println(hm);
 	}
